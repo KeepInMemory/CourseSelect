c07da035e91807b41bfc19018341b5b44acb97f2
KeepInMemory <54701705+KeepInMemory@users.noreply.github.com>
2019-12-11 14:59:00 +0800

    application更新

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Vim
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     config.i18n.default_locale = "zh-CN"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -23,6 +24,5 @@ module Vim
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
   end
 end
